c84915c3b563c39a8aa6496da4905cc96faebb6a
Kudyakov Artem <t3m8ch@yandex.ru>
2021-08-12 17:42:55 +0400

    refactor: Move events handling to separate module

---
+++ app/events_handling.py
@@ -0,0 +1,27 @@
+from typing import Callable
+
+from fastapi import FastAPI
+from sqlalchemy.ext.asyncio import create_async_engine
+
+from app.db.models import Base
+
+
+def create_startup_handler(app: FastAPI) -> Callable:
+    async def startup() -> None:
+        engine = create_async_engine(
+            "postgresql+asyncpg://localhost/fastapi"
+        )
+        app.state.alchemy_engine = engine
+
+        async with engine.begin() as conn:
+            await conn.run_sync(Base.metadata.drop_all)
+            await conn.run_sync(Base.metadata.create_all)
+
+    return startup
+
+
+def create_shutdown_handler(app: FastAPI) -> Callable:
+    async def shutdown() -> None:
+        await app.state.alchemy_engine.dispose()
+
+    return shutdown
