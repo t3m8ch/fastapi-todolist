114ea66ae7813e117d5c287818a7c068a6d66adb
Kudyakov Artem <t3m8ch@yandex.ru>
2021-08-12 16:51:36 +0400

    feat: Save SQLAlchemy engine in app.state

--- app/__main__.py
+++ app/__main__.py
@@ -1,10 +1,29 @@
 import uvicorn
 from fastapi import FastAPI
+from sqlalchemy.ext.asyncio import create_async_engine
 
+from app.db.models import Base
 from app.endpoints import include_routers
 
 app = FastAPI()
 include_routers(app)
+
+
+@app.on_event("startup")
+async def startup():
+    engine = create_async_engine(
+        "postgresql+asyncpg://localhost/fastapi"
+    )
+    app.state.alchemy_engine = engine
+
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+        await conn.run_sync(Base.metadata.create_all)
+
+
+@app.on_event("shutdown")
+async def shutdown():
+    await app.state.alchemy_engine.dispose()
 
 
 if __name__ == '__main__':
