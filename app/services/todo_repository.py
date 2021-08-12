b3a65df97dff1612c81ce02bfa50ce5e652b6871
Kudyakov Artem <t3m8ch@yandex.ru>
2021-08-12 16:52:02 +0400

    feat(db): Impl. create method in TodoRepository

---
+++ app/services/todo_repository.py
@@ -0,0 +1,24 @@
+from sqlalchemy import insert, select
+
+from app.db.models import Todo
+from app.schemas import OutTodo, CreatingTodo
+from app.services.base_repository import BaseRepository
+
+
+class TodoRepository(BaseRepository):
+    async def create(self, todo: CreatingTodo) -> OutTodo:
+        todo = (await self._session.execute(
+            insert(Todo).values(text=todo.text).
+            returning(Todo)
+        )).one()
+        await self._session.commit()
+
+        return _map_todo_to_pydantic(todo)
+
+
+def _map_todo_to_pydantic(table: Todo) -> OutTodo:
+    return OutTodo(
+        id=table.id,
+        text=table.text,
+        is_completed=table.is_completed
+    )
